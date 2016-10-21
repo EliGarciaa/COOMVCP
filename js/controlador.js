092370d8b6d34ab314a2f4d212fb477edd4636c8
eliseo Ajax <eajax@localhost.localdomain>
2016-10-20 20:49:15 -0600

    Nuevo Amen

--- js/controlador.js
+++
@@ -1,6 +0,0 @@
-function SaveData(){
-	var dato1 = document.getElementById("nombre").value;
-	var dato2 = document.getElementById("edad").value;
-	var datocaja = new GuardarUser(dato1, dato2);
-	datocaja.getGuardar();
-}
